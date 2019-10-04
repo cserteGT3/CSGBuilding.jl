605c26f35a284b42d6a4fc47459ee815bdfbbfe0
Tamás Cserteg <csertegt@gmail.com>
2019-10-04 13:10:30 +0200

    Files generated by PkgTemplates

---
+++ src/CSGBuilding.jl
@@ -0,0 +1,5 @@
+module CSGBuilding
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CSGBuilding
+using Test
+
+@testset "CSGBuilding.jl" begin
+    # Write your own tests here.
+end
