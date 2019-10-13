27a04168a411bf9de3b4a8e616a261da8adb18f2
Tamás Cserteg <csertegt@gmail.com>
2019-10-13 16:09:25 +0200

    more tests

--- test/implicitsurfaces.jl
+++ test/implicitsurfaces.jl
@@ -1,7 +1,19 @@
 @testset "evaluating sphere" begin
     sphere1 = ImplicitSphere(v0, 1.0)
-    @test sphere1.center == v0
-    @test sphere1.radius == 1.0
+    @test sphere1.center === v0
+    @test sphere1.radius === 1.0
+
+	sp2 = ImplicitSphere([0,0,0], 15.0)
+	@test sp2.center === SVector(0.0, 0, 0)
+	@test sp2.radius === 15.0
+
+	sp3 = ImplicitSphere([0,0,0], 15)
+	@test sp3.center === SVector(0, 0, 0)
+	@test sp3.radius === 15
+
+	sp4 = ImplicitSphere([0,0,0.], 15)
+	@test sp4.center === SVector(0., 0, 0)
+	@test sp4.radius === 15.0
 
     @test isapprox(value(evaluate(sphere1, v0)), -1)
 
@@ -41,8 +53,16 @@ end
     n1 = SVector(1,0,0.0);
     pl1 = ImplicitPlane(n1, n1)
 
-    @test pl1.normal == n1
-    @test pl1.point == n1
+    @test pl1.normal === n1
+    @test pl1.point === n1
+
+	pl2 = ImplicitPlane([0,0,0], [1,0,0])
+	@test pl2.point === SVector(0,0,0)
+	@test pl2.normal === SVector(1,0,0)
+
+	pl3 = ImplicitPlane([0.,0,0], [1,0,0])
+	@test pl3.point === SVector(0.,0,0)
+	@test pl3.normal === SVector(1.,0,0)
 
     @test isapprox(value(evaluate(pl1, n1)), 0)
     @test isapprox(value(evaluate(pl1, SVector(0,0,0))), -1)
@@ -66,9 +86,17 @@ end
 	n1 = SVector(0, 0, 1.0)
 	cyl = ImplicitCylinder(n1, v0, 1.)
 
-	@test cyl.center == v0
-	@test cyl.axis == n1
-	@test cyl.radius == 1.
+	@test cyl.center === v0
+	@test cyl.axis === n1
+	@test cyl.radius === 1.
+
+	cyl2 = ImplicitCylinder([0,0,1], [0,0,0], 0.00001)
+	@test cyl2.axis === SVector(0.0, 0, 1)
+	@test cyl2.center === SVector(0.0, 0, 0)
+
+	cyl3 = ImplicitCylinder([0,0,1.], [0,0,0], 15)
+	@test cyl3.axis === SVector(0.0, 0, 1)
+	@test cyl3.center === SVector(0.0, 0, 0)
 
 	@test isapprox(value(evaluate(cyl, [1, 0, 0])), 0)
 	@test isapprox(value(evaluate(cyl, [0, 1, 0])), 0)
@@ -98,6 +126,22 @@ end
 		@test normal(pl1, v0) == n1
 		@test normal(pl1, rand(SVector{3})) == n1
 		@test isapprox(dot(normal(pl1, rand(SVector{3})), [1,0,0]), 1)
+	end
+
+	@testset "cylinder" begin
+		n1 = SVector(0, 0, 1.0)
+		cyl = ImplicitCylinder(n1, v0, 1.)
+
+		cyl2 = ImplicitCylinder(n1, v0, 17)
+
+		@test normal(cyl, [1,0,0]) == normal(cyl2, [1,0,0])
+		@test normal(cyl, [1,0,0]) == [1, 0, 0]
+
+		@test normal(cyl, [3.14, 0, 5]) == [1, 0, 0]
+		@test normal(cyl, [-3.14, 0, -5]) == [-1, 0, 0]
+
+		@test normal(cyl, [0, 3.14, 5]) == [0, 1, 0]
+		@test normal(cyl, [0, -3.14, -5]) == [0, -1, 0]
 	end
 
 end
