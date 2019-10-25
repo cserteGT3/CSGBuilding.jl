e2d41d7af05d9b3567f6e3654909b0ae5392b9b0
Tamás Cserteg <csertegt@gmail.com>
2019-10-25 16:04:39 +0200

    function for normed covariance matrix

--- src/orientedbox.jl
+++ src/orientedbox.jl
@@ -2,3 +2,11 @@ function centroid(points)
     com = sum(points)
     return com/size(points, 1)
 end
+
+function normedcovmat(points)
+    @assert size(points,1) > 1
+    com = centroid(points)
+    vdiff = [p-com for p in points]
+    m = sum(vdiff*vdiff')
+    return m/(size(points,1)-1)
+end
