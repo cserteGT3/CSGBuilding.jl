ed8bf369bf5a67079d41651636e67ffadaaa895a
Tamás Cserteg <csertegt@gmail.com>
2019-10-28 07:47:56 +0100

    rm @threads from generated genetic

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -116,3 +116,83 @@ function geneticbuildtree(surfaces, points, normals, params)
     @info "Finished: $itermax iteration in $fint seconds."
     return population
 end
+
+function rawscorecached(tree, cpoints, cnormals, normals, params)
+    @unpack ϵ_d, α = params
+    # λ should be unpacked?
+    λ = log(size(cpoints, 1))
+    score = 0.
+    for i in eachindex(cpoints)
+        v, n = valueandnormal(tree, cpoints, cnormals, i)
+        d_i = v/ϵ_d
+        θ_i = acos(dot(n, normals[i]))/α
+        score += exp(-d_i^2) + exp(-θ_i^2)
+    end
+    return score - λ*treesize(tree)
+end
+
+function rankcachedpopulation(population, cpoints, cnormals, normals, params)
+    sum = 0.
+    score = Array{Float64,1}(undef, size(population))
+    normed = similar(score)
+
+    @threads for i in eachindex(population)
+    #for i in eachindex(population)
+        score[i] = rawscorecached(population[i], cpoints, cnormals, normals, params)
+    end
+    for i in eachindex(score)
+        normed[i] = 1/(1+score[i])
+        sum += normed[i]
+    end
+    for i in eachindex(score)
+        score[i] = normed[i]/sum
+    end
+    # this is the rescaled score
+
+    p = sortperm(score)
+    return population[p], score[p]
+end
+
+function cachedgeneticbuildtree(surfaces, points, normals, params)
+    @unpack itermax, maxdepth, populationsize = params
+    @unpack keepbestn = params
+    notifit = itermax > 10 ? div(itermax,10) : 1
+    cnodes, cvalues, cnormals = cachenodes(surfaces, points)
+    population = [randomcachedtree(cnodes, maxdepth) for _ in 1:populationsize]
+    npopulation = similar(population)
+    start_time = time_ns()
+    @info "Iteration in progress..."
+    for i in 1:itermax
+        if i%notifit == 0
+            @info "$i-th iteration"
+        end
+        population, _ = rankcachedpopulation(population, cvalues, cnormals, normals, params)
+        @debug "ranked population"
+        # save the best
+        npopulation[1:keepbestn] = population[1:keepbestn]
+        n = keepbestn+1
+        while true
+            # select 2 creatures by tournament
+            t1 = deepcopy(population[tournament(params)])
+            t2 = deepcopy(population[tournament(params)])
+            # crossover them
+            newc = crossover([t1, t2], params)
+            # mutate first
+            cv = mutatecached(newc[1], cnodes, params)
+            npopulation[n] = cv
+            n += 1
+            # finish if we have enough
+            n > populationsize && break
+            # mutate second
+            cv = mutatecached(newc[2], cnodes, params)
+            npopulation[n] = cv
+            n += 1
+            n > populationsize && break
+        end
+        # npopulation is populated with a new set of creature
+        population = npopulation
+    end
+    fint = trunc((time_ns() - start_time)/1_000_000_000, digits=2)
+    @info "Finished: $itermax iteration in $fint seconds."
+    return population, cached2normaltree(population[1], surfaces)
+end
--- src/genetic.jl
+++ src/genetic.jl
@@ -16,85 +16,6 @@
     selectionprob = 0.5
 end
 
-function rawscorecached(tree, cpoints, cnormals, normals, params)
-    @unpack ϵ_d, α = params
-    # λ should be unpacked?
-    λ = log(size(cpoints, 1))
-    score = 0.
-    for i in eachindex(cpoints)
-        v, n = valueandnormal(tree, cpoints, cnormals, i)
-        d_i = v/ϵ_d
-        θ_i = acos(dot(n, normals[i]))/α
-        score += exp(-d_i^2) + exp(-θ_i^2)
-    end
-    return score - λ*treesize(tree)
-end
-
-function rankcachedpopulation(population, cpoints, cnormals, normals, params)
-    sum = 0.
-    score = Array{Float64,1}(undef, size(population))
-    normed = similar(score)
-
-    @threads for i in eachindex(population)
-    #for i in eachindex(population)
-        score[i] = rawscorecached(population[i], cpoints, cnormals, normals, params)
-    end
-    for i in eachindex(score)
-        normed[i] = 1/(1+score[i])
-        sum += normed[i]
-    end
-    for i in eachindex(score)
-        score[i] = normed[i]/sum
-    end
-    # this is the rescaled score
-
-    p = sortperm(score)
-    return population[p], score[p]
-end
-
-
-function rawscorefunc(tree, cpoints, cnormals, normals, params, sem)
-    @unpack ϵ_d, α = params
-    # λ should be unpacked?
-    λ = log(size(cpoints, 1))
-    score = 0.
-    acquire(sem)
-    f = tree2func(tree)
-    release(sem)
-    for i in eachindex(cpoints)
-        res = Base.invokelatest(f, cpoints, i)::CachedResult
-        v = value(res)
-        n = normal(res, cnormals, i)
-        d_i = v/ϵ_d
-        θ_i = acos(dot(n, normals[i]))/α
-        score += exp(-d_i^2) + exp(-θ_i^2)
-    end
-    return score - λ*treesize(tree)
-end
-
-function rankcachedpopulationfunc(population, cpoints, cnormals, normals, params)
-    sum = 0.
-    score = Array{Float64,1}(undef, size(population))
-    normed = similar(score)
-
-    smphr = Semaphore(1)
-    @threads for i in eachindex(population)
-    #for i in eachindex(population)
-        score[i] = rawscorefunc(population[i], cpoints, cnormals, normals, params, smphr)
-    end
-    for i in eachindex(score)
-        normed[i] = 1/(1+score[i])
-        sum += normed[i]
-    end
-    for i in eachindex(score)
-        score[i] = normed[i]/sum
-    end
-    # this is the rescaled score
-
-    p = sortperm(score)
-    return population[p], score[p]
-end
-
 function crossover(creatures, params)
     @unpack χ, maxdepth = params
     if rand() >= χ
@@ -179,6 +100,48 @@ function tournament(params)
     return tourn[1]
 end
 
+function rawscorefunc(tree, cpoints, cnormals, normals, params, sem)
+    @unpack ϵ_d, α = params
+    # λ should be unpacked?
+    λ = log(size(cpoints, 1))
+    score = 0.
+    #acquire(sem)
+    f = tree2func(tree)
+    #release(sem)
+    for i in eachindex(cpoints)
+        res = Base.invokelatest(f, cpoints, i)::CachedResult
+        v = value(res)
+        n = normal(res, cnormals, i)
+        d_i = v/ϵ_d
+        θ_i = acos(dot(n, normals[i]))/α
+        score += exp(-d_i^2) + exp(-θ_i^2)
+    end
+    return score - λ*treesize(tree)
+end
+
+function rankcachedpopulationfunc(population, cpoints, cnormals, normals, params)
+    sum = 0.
+    score = Array{Float64,1}(undef, size(population))
+    normed = similar(score)
+
+    smphr = Semaphore(1)
+    #@threads for i in eachindex(population)
+    for i in eachindex(population)
+        score[i] = rawscorefunc(population[i], cpoints, cnormals, normals, params, smphr)
+    end
+    for i in eachindex(score)
+        normed[i] = 1/(1+score[i])
+        sum += normed[i]
+    end
+    for i in eachindex(score)
+        score[i] = normed[i]/sum
+    end
+    # this is the rescaled score
+
+    p = sortperm(score)
+    return population[p], score[p]
+end
+
 function cachedfuncgeneticbuildtree(surfaces, points, normals, params)
     @unpack itermax, maxdepth, populationsize = params
     @unpack keepbestn = params
@@ -194,50 +157,6 @@ function cachedfuncgeneticbuildtree(surfaces, points, normals, params)
         end
         population, _ = rankcachedpopulationfunc(population, cvalues, cnormals, normals, params)
         @debug "ranked population: $i. iteration"
-        # save the best
-        npopulation[1:keepbestn] = population[1:keepbestn]
-        n = keepbestn+1
-        while true
-            # select 2 creatures by tournament
-            t1 = deepcopy(population[tournament(params)])
-            t2 = deepcopy(population[tournament(params)])
-            # crossover them
-            newc = crossover([t1, t2], params)
-            # mutate first
-            cv = mutatecached(newc[1], cnodes, params)
-            npopulation[n] = cv
-            n += 1
-            # finish if we have enough
-            n > populationsize && break
-            # mutate second
-            cv = mutatecached(newc[2], cnodes, params)
-            npopulation[n] = cv
-            n += 1
-            n > populationsize && break
-        end
-        # npopulation is populated with a new set of creature
-        population = npopulation
-    end
-    fint = trunc((time_ns() - start_time)/1_000_000_000, digits=2)
-    @info "Finished: $itermax iteration in $fint seconds."
-    return population, cached2normaltree(population[1], surfaces)
-end
-
-function cachedgeneticbuildtree(surfaces, points, normals, params)
-    @unpack itermax, maxdepth, populationsize = params
-    @unpack keepbestn = params
-    notifit = itermax > 10 ? div(itermax,10) : 1
-    cnodes, cvalues, cnormals = cachenodes(surfaces, points)
-    population = [randomcachedtree(cnodes, maxdepth) for _ in 1:populationsize]
-    npopulation = similar(population)
-    start_time = time_ns()
-    @info "Iteration in progress..."
-    for i in 1:itermax
-        if i%notifit == 0
-            @info "$i-th iteration"
-        end
-        population, _ = rankcachedpopulation(population, cvalues, cnormals, normals, params)
-        @debug "ranked population"
         # save the best
         npopulation[1:keepbestn] = population[1:keepbestn]
         n = keepbestn+1
